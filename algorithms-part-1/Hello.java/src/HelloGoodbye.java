ecadf21b9f74a518aeed6c8efdddfd83695e512b
sojha2 <122489814+sojha2@users.noreply.github.com>
2023-05-30 12:04:40 -0600

    Add some files

---
+++ algorithms-part-1/Hello.java/src/HelloGoodbye.java
@@ -0,0 +1,11 @@
+import edu.princeton.cs.algs4.StdOut;
+
+public class HelloGoodbye {
+  public static void main(String[] args) {
+    String person1 = args[0];
+    String person2 = args[1];
+    StdOut.println("Hello " + person1 + " and " + person2 + ".");
+    StdOut.println("Goodbye " + person1 + " and " + person2 + ".");
+    //      "args": ["First", "Second"]      
+  }
+}
